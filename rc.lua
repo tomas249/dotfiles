34cf843c6ae2cca30e5271f4e7222374e3da3859
tomas249 <tomastvmv@gmail.com>
2023-07-03 17:53:05 +0200

    add imwheel autostart

--- rc.lua
+++ rc.lua
@@ -385,3 +385,5 @@ end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+
+awful.spawn.with_shell("/usr/bin/imwheel -k --buttons '4 5'")
