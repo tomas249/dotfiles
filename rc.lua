7bc5adc1aae6c07cb5df29f2ff3eaf909b8e3325
tomas249 <tomastvmv@gmail.com>
2023-07-08 03:07:00 +0200

    update

--- rc.lua
+++ rc.lua
@@ -353,6 +353,7 @@ awful.rules.rules = {
           "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
           "Wpa_gui",
           "veromix",
+          "xarchiver", -- File chooser / unzipper
           "xtightvncviewer"},
 
         -- Note that the name property shown in xprop might be set slightly after creation of the client
